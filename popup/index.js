1ba6fde1ae8cde5494060d6a4a14d73f13329aea
김진범 <lljrwq123@gmail.com>
2024-10-22 00:10:48 +0900

    refactor:  동적랜더에서 정적랜더로 변경

--- popup/index.js
+++ popup/index.js
@@ -1,6 +1,5 @@
-const pnRules = { p1: "필수", p2: "적극 고려", p3: "의견, 제안" };
-
 const form = document.querySelector("form");
+
 form.addEventListener("submit", (e) => {
   e.preventDefault();
   const formData = new FormData(form);
@@ -15,32 +14,11 @@ function saveEmoji(emojiMap) {
 
 function getValues() {
   chrome.storage.local.get().then((data) => {
-    Object.entries(pnRules).forEach(([name]) => {
-      const input = document.querySelector(`[name='${name}']`);
-      input.value = data[name] || "";
+    Object.keys(data).forEach((key) => {
+      const input = document.querySelector(`[name='${key}']`);
+      input.value = data[key] || "";
     });
   });
 }
 
-function generateLabeledInput(name, label, desc) {
-  const input = document.createElement("input");
-  input.type = "text";
-  input.name = name;
-  input.autocomplete = "off";
-  input.placeholder = "대치어 입력";
-  input.setAttribute("list", "emoji-list");
-
-  const labelElement = document.createElement("label");
-  labelElement.textContent = label + ": " + desc;
-  labelElement.appendChild(input);
-
-  return labelElement;
-}
-function appendPnInputs() {
-  Object.entries(pnRules).forEach(([name, desc]) => {
-    form.appendChild(generateLabeledInput(name, name, desc));
-  });
-}
-
-appendPnInputs();
 getValues();
